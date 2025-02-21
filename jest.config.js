232a8b25adcfb615b9bb788ae24f4158a7d72b2c
itolog <paukanlove@gmail.com>
2025-02-21 15:44:05 +0200

    change jest config

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,14 @@
 export default {
+  verbose: true,
+  collectCoverage: true,
+  collectCoverageFrom: ["<rootDir>/src/**/*.{js,ts,tsx}"],
+  // coverageThreshold: {
+  //   global: {
+  //     branches: 80,
+  //     functions: 80,
+  //     lines: 80,
+  //   },
+  // },
   preset: "ts-jest",
   testEnvironment: "jest-environment-jsdom",
   transform: {
--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { useEffect } from "react";
 import "./App.css";
 
-import viteLogo from "/vite.svg";
 import { sortingOddWithoutEven } from "./tasks/sortingOddWithoutEven.ts";
 
 function App() {
@@ -14,11 +13,6 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-      </div>
       <h1>Vite + React</h1>
     </>
   );
